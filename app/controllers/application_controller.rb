bbb59e66e38d0b02d1a84fa00b624581f46db899
Fred Higgins <higgins.fred.s@gmail.com>
2022-06-22 21:59:25 -0700

    commit tree 6630d1f7ad8878678ae5b2d1f2e67f781e992d2b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  before_action :authorize
+
+  
+  private
+
+  def authorize
+    @user = User.find_by(id: session[:user_id])
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @user
+  end
+
+
+
 end
